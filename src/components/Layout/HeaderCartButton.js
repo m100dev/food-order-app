44ec1dc0672e45bd453a7da4cd423410c1b14c1c
Marc Bastien <m100.dev@gmail.com>
2022-11-28 11:21:19 -0500

    made some updates to eslint and prettier configs. Updated cart button for Header component.

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,13 +1,16 @@
-import React from 'react'
+import React from 'react';
+
+import CartIcon from '../Cart/CartIcon';
+import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = () => {
   return (
-    <button>
-      <span>
-
+    <button className={classes.button}>
+      <span className={classes.icon}>
+        <CartIcon />
       </span>
-      <span></span>
-      <span></span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>3</span>
     </button>
   )
 }
