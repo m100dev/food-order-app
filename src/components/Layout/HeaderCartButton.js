103a4692dbe46fa41744df4944130f3d2afe191b
Marc Bastien <m100.dev@gmail.com>
2022-11-28 10:48:35 -0500

    initial commit

---
+++ src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const HeaderCartButton = () => {
+  return (
+    <button>
+      <span>
+
+      </span>
+      <span></span>
+      <span></span>
+    </button>
+  )
+}
+
+export default HeaderCartButton;
