6792e00695f647706a63a2b899a4a3c3e4979b30
Marc Bastien <m100.dev@gmail.com>
2022-11-28 13:58:03 -0500

    Created meal item form component and an input wrapper component for selecting meal quantity to eventually add the meal to the cart

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import MealItemForm from './MealItemForm';
 import classes from './MealItem.module.css';
 
 const MealItem = (props) => {
@@ -12,7 +13,7 @@ const MealItem = (props) => {
         <div className={classes.description}>{props.description}</div>
         <div className={classes.price}>{price}</div>
       </div>
-      <div></div>
+      <MealItemForm />
     </li>
   );
 };
